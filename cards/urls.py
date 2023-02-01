755c2f4d774196a776b81a042a6241049ba53e50
“nathanhwyoung” <nhwilcox@gmail.com>
2023-02-01 14:26:30 -0800

    add details element, configure views

--- cards/urls.py
+++ cards/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import CardListView, HomePageView, random_card
+from .views import CardListView, HomePageView, practice
 
 urlpatterns = [
-    # path("", random_card, name="random-card"),
     path("", HomePageView.as_view(), name="home"),
+    path("practice/", practice, name="practice"),
     path("all_cards/", CardListView.as_view(), name="card-list"),
 ]
--- cards/views.py
+++ cards/views.py
@@ -2,16 +2,11 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView
 from django.shortcuts import render
 from random import randint
-from .models import Card, Site
+from .models import Card
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
-
-
-# we don't have this yet
-# class AboutPageView(TemplateView):
-#     template_name = "about.html"
 
 
 class CardListView(ListView):
@@ -19,9 +14,10 @@ class CardListView(ListView):
     queryset = Card.objects.all()
 
 
-def random_card(request):
+def practice(request):
+    # this view will need to exclude cards that have been marked as completed for the active user
     total_cards = Card.objects.count()
     random_index = randint(0, total_cards - 1)
     random_card = Card.objects.all()[random_index]
     context = {"random_card": random_card}
-    return render(request, "cards/random.html", context)
+    return render(request, "cards/question.html", context)
